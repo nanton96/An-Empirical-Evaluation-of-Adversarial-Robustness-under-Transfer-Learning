aabe15bfa4abc1c8f5e50252461002834449bf02
Stamatios-Korres <timoskorres@hotmail.com>
2019-02-10 22:13:18 +0000

    training code improved

--- resnetCIFAR10.py
+++ resnetCIFAR10.py
@@ -7,7 +7,7 @@ import torch.optim as optim
 import torch.nn as nn
 import torch.backends.cudnn as cudnn
 import torch.nn.functional as F
-from utils import progress_bar
+# from utils import progress_bar
 import os
 import argparse
 import logging
