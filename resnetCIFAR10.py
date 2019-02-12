5f8e688c1dd423ef45f3338f49060b857f776264
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-02-12 13:53:45 +0000

    correct cifar10 transforms

--- resnetCIFAR10.py
+++ resnetCIFAR10.py
@@ -32,26 +32,32 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
 # The output of torchvision datasets are PILImage images of range [0, 1].
 # We transform them to Tensors of normalized range [-1, 1].
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                        download=False, transform=transform)
+                                        download=False, transform=transform_train)
+
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=500,
                                           shuffle=True, num_workers=4)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                       download=False, transform=transform)
+                                       download=False, transform=transform_test)
 testloader = torch.utils.data.DataLoader(testset, batch_size=100,
                                          shuffle=False, num_workers=4)
 
 logging.info("Both datasets were downloaded")
-classes = ('plane', 'car', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-net = resnet50(pretrained=False)
-net = nn.DataParallel(net, device_ids=None)
 
+net = resnet50(pretrained=False)
 
 def train(epoch,trainloader):
     net.train()
@@ -112,9 +118,9 @@ def test(epoch,testloader):
 
 if device == 'cuda':
     cudnn.benchmark = True
+    net = nn.DataParallel(net, device_ids=None)
 
 #Define a Loss function and optimize
-
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
 scheduler = optim.lr_scheduler.StepLR(optimizer=optimizer,step_size=25,gamma=0.1)
