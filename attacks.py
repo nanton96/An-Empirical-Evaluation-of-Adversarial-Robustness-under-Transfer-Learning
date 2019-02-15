58fdbc1e0862aaf102183d5232fcd0681f7d665f
timos <s1877727@sms.ed.ac.uk>
2019-02-15 17:35:40 +0000

    Attacking with multiple epsilons

--- attacks.py
+++ attacks.py
@@ -45,8 +45,7 @@ class attacks():
                                                           rng=rng,
                                                           flatten=False)  # initialize our rngs using the argument set seed
             num_output_classes = train_data.num_classes
-            return {"train_data": train_data, "val_data": val_data, "test_data": test_data,
-                    "num_output_classes": num_output_classes}
+            return {"train_data": train_data, "val_data": val_data, "test_data": test_data,"num_output_classes": num_output_classes}
         elif dataset_name == 'cifar10':
             transform_train = transforms.Compose([
                 transforms.RandomCrop(32, padding=4),
@@ -69,7 +68,7 @@ class attacks():
             testset = data_providers.CIFAR10(root='data', set_name='test', download=True, transform=transform_test)
             test_data = torch.utils.data.DataLoader(testset, batch_size=1, shuffle=False, num_workers=2)
         
-            classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+            # classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
             num_output_classes = 10
             return {"train_data": train_data, "val_data": val_data, "test_data": test_data,
                     "num_output_classes": num_output_classes}
@@ -100,7 +99,7 @@ class attacks():
             
     def store_adv_examples(self):
         with open("adv_examples","w+") as f:
-            json.dump(self.stats, f)
+            json.dump(self.adv_examples, f)
         
     def get_adv_examples(self):
         return self.adv_examples
@@ -138,61 +137,66 @@ class fgsm(attacks):
         # Initialization
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         model = self.model
-        correct = 0
-        epsilon = self.epsilon
         test_loader = self.dataset["test_data"]
-       
+
         if torch.cuda.is_available():
             model = torch.nn.DataParallel(model)
         model = model.to(device)
-        print("FGSM attacks starts")
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            data.requires_grad = True
-        
-            output = model(data)
-            init_pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
-        
-            # If the initial prediction is wrong, dont bother attacking, just move on
-            if init_pred.item() != target.item():
-                continue
-        
-            loss = F.nll_loss(output, target)
-            model.zero_grad()
-            loss.backward()
-        
-            # Collect datagrad
-            data_grad = data.grad.data
-        
-            perturbed_data = self.fgsm_attack(data, epsilon, data_grad)
-        
-            # Re-classify the perturbed image
-            output = model(perturbed_data)
-        
-            # Check for success
-            final_pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
-            if final_pred.item() == target.item():
-                correct += 1
-                # Special case for saving 0 epsilon examples
-                if (epsilon == 0) and (len( self.adv_examples) < 5):
-                    adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
-                    self.adv_examples.append((init_pred.item(), final_pred.item(), adv_ex))
-            else:
-                # Save some adv examples for visualization later
-                if len( self.adv_examples) < 5:
-                    adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
-                    self.adv_examples.append((init_pred.item(), final_pred.item(), adv_ex))
-        
-            # Calculate final accuracy for this epsilon
-        final_acc = correct / float(len(test_loader))
-        self.stats['epsilon'].append(epsilon)
-        self.stats['accuracy'].append(final_acc)
-        print("Epsilon: {}\tTest Accuracy = {} / {} = {}".format(epsilon, correct, len(test_loader), final_acc))
+        for epsilon in self.epsilon:
+            self.adv_examples = []
+            print("attacking with epsilon:", epsilon)
+            correct = 0
+            for data, target in test_loader:
+                data, target = data.to(device), target.to(device)
+                data.requires_grad = True
+            
+                output = model(data)
+                init_pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
+            
+                # If the initial prediction is wrong, dont bother attacking, just move on
+                if init_pred.item() != target.item():
+                    continue
+            
+                loss = F.nll_loss(output, target)
+                model.zero_grad()
+                loss.backward()
+            
+                # Collect datagrad
+                data_grad = data.grad.data
+            
+                perturbed_data = self.fgsm_attack(data, epsilon, data_grad)
+            
+                # Re-classify the perturbed image
+                output = model(perturbed_data)
+            
+                # Check for success
+                final_pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
+                if final_pred.item() == target.item():
+                    correct += 1
+                    # Special case for saving 0 epsilon examples
+                    if (epsilon == 0) and (len( self.adv_examples) < 5):
+                        adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                        self.adv_examples.append((init_pred.item(), final_pred.item(), adv_ex))
+                else:
+                    # Save some adv examples for visualization later
+                    if len( self.adv_examples) < 5:
+                        adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
+                        self.adv_examples.append((init_pred.item(), final_pred.item(), adv_ex))
+            
+                # Calculate final accuracy for this epsilon
+            final_acc = correct / float(len(test_loader))
+            self.stats['epsilon'].append(epsilon)
+            self.stats['accuracy'].append(final_acc)
+            print("Epsilon: {}\tTest Accuracy = {} / {} = {}".format(epsilon, correct, len(test_loader), final_acc))
+            self.store_adv_examples()
+        self.save_attack_statistic()
+        return final_acc
+
+    def save_attack_statistic(self):
         with open(self.stats_paths+self.experiment_name+'.csv', 'w+') as fp:
             json.dump(self.stats, fp)
     
         # Return the accuracy and an adversarial example
-        return final_acc
 
     def fgsm_attack(self,image, epsilon, data_grad):
         # print("Applying FGSM to image")
@@ -208,7 +212,7 @@ class fgsm(attacks):
         # Return the perturbed image
         return perturbed_image
 
-att = fgsm(epsilon=0.2,model_name="resnet50",dataset_name="cifar10")
+att = fgsm(epsilon=[0.05,0.1,0.2,0.03],model_name="resnet50",dataset_name="cifar10")
 att.attack()
 att.store_adv_examples()
 
