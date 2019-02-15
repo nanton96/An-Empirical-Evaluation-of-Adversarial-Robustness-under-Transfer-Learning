fd61493db166993e31cf8456e60e2386668eefb7
timos <s1877727@sms.ed.ac.uk>
2019-02-15 14:50:40 +0000

    Stupid bugs

--- scripts/run.sh
+++ scripts/run.sh
@@ -24,6 +24,7 @@ export MODELS_DIR=/disk/scratch/ibm_prize_winners/models
 
 # Activate the relevant virtual environment
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
-python FGSM.py
+cd ..
+python attacks.py
 
 rsync -ua --progress /disk/scratch/ibm_prize_winners/models/ /home/${STUDENT_ID}/mlpcw4/models/
