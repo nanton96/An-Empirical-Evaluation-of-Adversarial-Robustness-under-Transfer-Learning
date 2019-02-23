017c22f6c14d0c38b91ed751069fc8d4d41054b4
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-02-23 16:34:44 +0200

    densenet

--- cluster_scripts/transfer_densenet121.sh
+++ cluster_scripts/transfer_densenet121.sh
@@ -38,7 +38,7 @@ source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 # cd ..
 #mkdir experiments_results
 
-python transfer.py  --batch_size 100 --continue_from_epoch -1 --seed 0 \
-					--lr ${LR} --model resnet56 --num_epochs 25 \
+python transfer.py  --batch_size 128 --continue_from_epoch -1 --seed 0 \
+					--lr ${LR} --model densenet121 --num_epochs 25 \
 					--use_gpu "True" --gpu_id="0" --weight_decay_coefficient 0.0000 \
 					--dataset_name "cifar100" --source_net 'cifar10'
