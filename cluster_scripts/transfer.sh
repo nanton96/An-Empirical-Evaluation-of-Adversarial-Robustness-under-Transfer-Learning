89714d61a7bce51fe3bf74ecb8e55f68d7a0c5eb
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-02-27 19:13:47 +0000

    bug

--- cluster_scripts/transfer.sh
+++ cluster_scripts/transfer.sh
@@ -8,7 +8,7 @@ TARGET=cifar10
 for MODEL in resnet56 densenet121; do
 	for FEATURE_EXTRACTION in True False;do 
 		for LR in 0.100 0.010 0.001; do
-			cat  transfer.sh.template | sed -e "s/LR/$LR/;s/MODEL/$MODEL/;s/SOURCE/$SOURCE/;s/TARGET/$TARGET/;s/FEATURE_EXTRACTION/$FEATURE_EXTRACTION/" > "transfer/job${var}.sh"
+			cat  transfer.sh.template | sed -e "s/LR/$LR/;s/MODELNAME/$MODEL/;s/SOURCE/$SOURCE/;s/TARGET/$TARGET/;s/FEATURE_EXTRACTION/$FEATURE_EXTRACTION/" > "transfer/job${var}.sh"
 			chmod +x "transfer/job${var}.sh"
 			var=$((var+1))
 		done
