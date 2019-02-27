8f70796ddcb616603fe4c3b617664a1b08aad369
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-02-27 16:56:01 +0000

    fix num classes

--- transfer.py
+++ transfer.py
@@ -24,7 +24,7 @@ experiment_name = 'transfer_%s_%s_to_%s_lr_%.5f' % (args.model, args.source_net,
 logging.info('Experiment name: %s' %experiment_name)
 
 num_output_classes, train_data,val_data,test_data = getDataProviders(dataset_name=args.dataset_name, rng = rng, batch_size = args.batch_size)
-num_original_classes = 10
+num_original_classes = 10 if args.source_net == 'cifar10' else 100
 
 model_path =os.path.join(MODELS_DIR, "%s_%s/saved_models/train_model_best" % (args.model, args.source_net))
 logging.info('Loading %s model from %s' % (args.source_net, model_path))
