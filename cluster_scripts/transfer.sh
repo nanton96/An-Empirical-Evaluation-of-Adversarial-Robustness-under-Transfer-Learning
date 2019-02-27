0cfa2acd6edfc12c7fe4e70a263f29e664392308
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-02-27 19:40:44 +0000

    transfer standard

--- cluster_scripts/transfer.sh
+++ cluster_scripts/transfer.sh
@@ -15,5 +15,20 @@ for MODEL in resnet56 densenet121; do
 	done
 done
 
+SOURCE=cifar10
+TARGET=cifar100
+
+# resnet / last layers
+
+for MODEL in resnet56 densenet121; do
+	for FEATURE_EXTRACTION in True False;do 
+		for LR in 0.100 0.010 0.001; do
+			cat  transfer.sh.template | sed -e "s/LR/$LR/;s/MODELNAME/$MODEL/;s/SOURCE/$SOURCE/;s/TARGET/$TARGET/;s/FEATURE_EXTRACTION/$FEATURE_EXTRACTION/" > "transfer/job${var}.sh"
+			chmod +x "transfer/job${var}.sh"
+			var=$((var+1))
+		done
+	done
+done
+
 cd transfer
-python run_jobs_simple.py --num_parallel_jobs=12 --total_epochs=25
+python run_jobs_simple.py --num_parallel_jobs=24 --total_epochs=25
