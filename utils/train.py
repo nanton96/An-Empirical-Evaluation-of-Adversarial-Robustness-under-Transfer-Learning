02132f10991d648de7e60eb666dbf0882df899dc
Stamatios-Korres <timoskorres@hotmail.com>
2019-03-04 00:28:27 +0000

    Fixing bug at fgsm

--- utils/train.py
+++ utils/train.py
@@ -30,8 +30,8 @@ def adv_train(X, y, model, criterion, adversary):
     model_cp.eval()
     
     adversary.model = model_cp
-    
-    X = X.cpu().numpy()
+    if torch.cuda.is_available():
+        X = X.cpu()
     X_adv = adversary.perturb(X.numpy(), y)
 
     return torch.from_numpy(X_adv)
