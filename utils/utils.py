81c36db6099db75eaa2fe6db507cd510d8e6120b
timos <s1877727@sms.ed.ac.uk>
2019-03-06 16:37:27 +0000

    Update

--- utils/utils.py
+++ utils/utils.py
@@ -51,7 +51,11 @@ def to_var(x, requires_grad=False, volatile=False):
     """
     if torch.cuda.is_available():
         x = x.cuda()
-    return Variable(x, requires_grad=requires_grad, volatile=volatile)
+    if volatile:
+        with torch.no_grad():
+            return Variable(x, requires_grad=requires_grad)
+    else:
+        return Variable(x, requires_grad=requires_grad, volatile=volatile)
 
 
 def pred_batch(x, model):
