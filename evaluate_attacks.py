28ed60fa2b2fd8509ef42490b951991259e5ea22
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-03-07 12:21:35 +0000

    evaluate works

--- evaluate_attacks.py
+++ evaluate_attacks.py
@@ -12,7 +12,7 @@ from utils.arg_extractor import get_args
 from utils.experiment_builder import ExperimentBuilder
 from utils.storage_utils import dict_load
 from utils.attacks import FGSMAttack,LinfPGDAttack
-from utils.utils import load_net
+from utils.utils import load_net, test, attack_over_test_data
 from utils.train import adv_train
 
 DATA_DIR=os.environ['DATA_DIR']
@@ -50,9 +50,9 @@ torch.manual_seed(seed=0) # sets pytorch's seed
         # # calculate accuracy and store in dictionary under key 'model_attack_black_box(source)_acc'
 
 models = ['resnet56'] # 'densenet121']
-network_names = ['cifar10', 'cifar100', 'cifar100_to_cifar10']
+network_names = ['cifar10'] #, 'cifar100', 'cifar100_to_cifar10']
 robust_to = ["", ]# "_fgsm", "_pgd"]
-attacks = ['fgsm', 'pgd']
+attacks = ['fgsm'] #, 'pgd']
 
 if torch.cuda.is_available():  # checks whether a cuda gpu is available and whether the gpu flag is True
     device = torch.device('cuda')  # sets device to be cuda
@@ -61,46 +61,6 @@ else:
     print("use CPU")
     device = torch.device('cpu')  # sets the device to be CPU
 
-
-def run_adv_evaluation(net,adversary,x,y):
-        net.eval()  # sets the system to validation mode
-
-        validaton_stat = {'clean_acc':0, 'clean_loss': 0, 'adv_acc':0, 'adv_loss': 0 }
-        if len(y.shape) > 1:
-            y = np.argmax(y, axis=1)  # convert one hot encoded labels to single integer labels
-        if type(x) is np.ndarray:
-            x, y = torch.Tensor(x).float().to(device=device), torch.Tensor(y).long().to(
-            device=device)  # convert data to pytorch tensors and send to the computation device
-        x = x.to(device)
-        y = y.to(device)
-
-
-        out = net.model(x)
-        loss = F.cross_entropy(input=out, target=y)
-        _,predicted = torch.max(out.data, 1)  
-        accuracy = np.mean(list(predicted.eq(y.data).cpu()))
-        validaton_stat['clean_acc']  = accuracy
-        validaton_stat['clean_loss'] = loss
-        
-        # Prevent label leaking, by using most probable state
-        y_pred  = pred_batch(x,net.model)
-
-        # Create corresponding adversarial examples for training 
-
-        # adversary = net.attacker(epsilon = 0.125)
-        x_adv = adv_train(x,y_pred, net.model,nn.CrossEntropyLoss(),adversary)
-        x_adv_var = to_var(x_adv)
-        out = net.model(x_adv_var)
-        _,predicted = torch.max(out.data, 1)  
-        adv_acc = np.mean(list(predicted.eq(y.data).cpu()))
-        loss_adv =  F.cross_entropy(out, y.data)
-
-        validaton_stat['adv_acc']  = adv_acc
-        validaton_stat['adv_loss'] = loss_adv
-
-        loss = (loss + loss_adv) / 2   
-        accuracy =  (accuracy + adv_acc)/2
-        return loss.data.detach().cpu().numpy(), accuracy, validaton_stat
 
 for network in network_names:
     dataset_name = {'cifar10': 'cifar10', 'cifar100': 'cifar100', 'cifar100_to_cifar10': 'cifar10'}[network]
@@ -117,5 +77,5 @@ for network in network_names:
             net = load_net(model, model_path, num_output_classes)
             
             # Attack FGSM
-            fgsmAttack = FGSMAttack(net, epsilon=0.25)
-            run_adv_evaluation(net,adversary,x,y)
+            fgsmAttack = FGSMAttack(net, epsilon=0.0)
+            acc = attack_over_test_data(model=net, adversary=fgsmAttack, param=None, loader_test=test_data, oracle=None)
