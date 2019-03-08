caa865aef35fbdbd3d95a437dd3b10a7669bda5c
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-03-08 12:55:04 +0000

    Update

--- cluster_scripts/resnet56_cifar100.sh
+++ cluster_scripts/resnet56_cifar100.sh
@@ -2,9 +2,9 @@
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
 #SBATCH --partition=Standard
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
 #SBATCH --mem=12000  # memory in Mb
-#SBATCH --time=0-08:00:00
+#SBATCH --time=0-04:00:00
 
 export CUDA_HOME=/opt/cuda-9.0.176.1/
 
@@ -38,7 +38,7 @@ source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 cd ..
 mkdir experiments_results
 
-python train.py --batch_size 100 --continue_from_epoch -1 --seed 0 \
+python train.py --batch_size 800 --continue_from_epoch -1 --seed 0 \
                                                       --lr 0.1 --model resnet56\
                                                       --num_epochs 200 --experiment_name 'resnet56_cifar100' \
                                                       --use_gpu "True" --gpu_id "0" --weight_decay_coefficient 0.00005 \
