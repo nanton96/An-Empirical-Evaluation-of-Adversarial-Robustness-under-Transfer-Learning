4358bbf6b0db661a3f405b5b46989ca8d1ddc1fd
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-03-09 12:55:14 +0000

    Update

--- cluster_scripts/resnet56_cifar100_pgd.sh
+++ cluster_scripts/resnet56_cifar100_pgd.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=LongJobs
 #SBATCH --gres=gpu:1
 #SBATCH --mem=12000  # memory in Mb
-#SBATCH --time=0-7:59:59
+#SBATCH --time=0-23:59:59
 
 export CUDA_HOME=/opt/cuda-9.0.176.1/
 
