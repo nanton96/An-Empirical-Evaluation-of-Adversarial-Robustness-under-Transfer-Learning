0ea1f822a9b498a6d36fc2eed10412f84427020d
timos <s1877727@sms.ed.ac.uk>
2019-03-17 01:34:10 +0000

    trying to transfer robust network

--- cluster_scripts/densesnet121_cifar10/densenet121_cifar10.sh
+++ cluster_scripts/densesnet121_cifar10/densenet121_cifar10.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
-#SBATCH --partition=Short
+#SBATCH --partition=Standard
 #SBATCH --gres=gpu:1
 #SBATCH --mem=12000  # memory in Mb
-#SBATCH --time=0-03:59:00
+#SBATCH --time=0-07:59:00
 
 export CUDA_HOME=/opt/cuda-9.0.176.1/
 
@@ -32,19 +32,15 @@ mkdir -p ${TMP}/datasets/
 export DATASET_DIR=${TMP}/datasets/
 # Activate the relevant virtual environment:
 
+
+
 rsync -ua --progress /home/${STUDENT_ID}/mlpcw4/data/ /disk/scratch/${STUDENT_ID}/data
 
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 cd ..
-
-
-python transfer.py --batch_size 100 --continue_from_epoch -1 --seed 0 \
-                 --adv_train True \
-                 --num_epochs 50 \
-                 --adversary "fgsm" \
-                 --lr 0.1 --model 'resnet56' \
-                 --source_net cifar100 \
-                 --experiment_name 'adv_transfer_fgsm_resnet56_cifar100_to_10' \
-                 --use_gpu True --gpu_id "0" --weight_decay_coefficient 0.00005 \
-                 --unfrozen_layers 6 \
-                 --dataset_name "cifar10"
+cd ..
+python train.py --batch_size 100 --continue_from_epoch -1 --seed 0 \
+                                                    --lr 0.1 --model densenet121\
+                                                      --num_epochs 200 --experiment_name 'densenet121_cifar10' \
+                                                      --use_gpu "True" --gpu_id "0" --weight_decay_coefficient 0.00005 \
+                                                      --dataset_name "cifar10"
