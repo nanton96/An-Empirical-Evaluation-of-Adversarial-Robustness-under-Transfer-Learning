b309543bd05d9e091c1314c95322af13f1b59495
timos <s1877727@sms.ed.ac.uk>
2019-03-19 13:37:30 +0000

    Deploying attacks

--- transfer.py
+++ transfer.py
@@ -29,15 +29,18 @@ experiments =  {
         'transfer_densenet121_nat_fgsm':    ('densenet121_cifar100',      'densenet121', 'fgsm', True, 6),
         'transfer_densenet121_nat_nat':     ('densenet121_cifar100',      'densenet121', 'nat', False, 6),
 
+
+        'transfer_densenet121_pgd_pgd':   ('densenet121_cifar100_pgd', 'densenet121', 'pgd', True, 6), 
+        'transfer_densenet121_pgd_nat':   ('densenet121_cifar100_pgd', 'densenet121', 'nat', False, 6),
+        'transfer_densenet121_nat_pgd':   ('densenet121_cifar100',     'densenet121', 'pgd', True, 6),
+        
+
         'transfer_resnet56_fgsm_fgsm':      ('resnet56_cifar100_fgsm', 'resnet56', 'fgsm', True, 6), 
         'transfer_resnet56_fgsm_nat':       ('resnet56_cifar100_fgsm', 'resnet56', 'nat', False, 6),
         'transfer_resnet56_nat_fgsm':       ('resnet56_cifar100',      'resnet56', 'fgsm', True, 6),
         'transfer_resnet56_nat_nat':        ('resnet56_cifar100',      'resnet56', 'nat', False, 6),
 
 
-        'transfer_densenet121_pgd_pgd':   ('densenet121_cifar100_pgd', 'densenet121', 'pgd', True, 6), 
-        'transfer_densenet121_pgd_nat':   ('densenet121_cifar100_pgd', 'densenet121', 'nat', False, 6),
-        'transfer_densenet121_nat_pgd':   ('densenet121_cifar100',     'densenet121', 'pgd', True, 6),
         
 
         'transfer_resnet56_pgd_pgd':      ('resnet56_cifar100_pgd',  'resnet56', 'pgd', True, 6), 
