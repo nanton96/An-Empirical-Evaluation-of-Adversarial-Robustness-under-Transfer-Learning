cd47bbca4f13bbaf4179e6cfc17ebb1a397a76b4
timos <s1877727@sms.ed.ac.uk>
2019-03-19 16:22:19 +0000

    Almost done

--- evaluate_attacks.py
+++ evaluate_attacks.py
@@ -55,17 +55,17 @@ trained_networks =  {
                     
                     # 'transfer_densenet121_fgsm_fgsm' : ('cifar10','densenet121'),
                     # 'transfer_densenet121_nat_fgsm': ('cifar10', 'densenet121'),
-                    'transfer_densenet121_pgd_pgd': ('cifar10', 'densenet121'),
-                    'transfer_densenet121_pgd_nat': ('cifar10', 'densenet121'),
-                    'transfer_densenet121_nat_pgd': ('cifar10', 'densenet121'),
-                    'transfer_densenet121_nat_nat': ('cifar100', 'densenet121'), 
+                    # 'transfer_densenet121_pgd_pgd': ('cifar10', 'densenet121'),
+                    # 'transfer_densenet121_pgd_nat': ('cifar10', 'densenet121'),
+                    # 'transfer_densenet121_nat_pgd': ('cifar10', 'densenet121'),
+                    'transfer_densenet121_nat_nat': ('cifar10', 'densenet121'), 
                     
                     # 'transfer_resnet56_fgsm_fgsm': ('cifar10', 'resnet56'),
                     'transfer_resnet56_pgd_pgd': ('cifar10', 'resnet56'),
                     'transfer_resnet56_pgd_nat': ('cifar10', 'resnet56'),
                     # 'transfer_resnet56_nat_fgsm': ('cifar10', 'resnet56'),
                     'transfer_resnet56_nat_pgd': ('cifar10', 'resnet56'),
-                    'transfer_resnet56_nat_nat': ('cifar100', 'resnet56')
+                    'transfer_resnet56_nat_nat': ('cifar10', 'resnet56')
 
 
                     # 'densenet121_cifar100_pgd': ('cifar100','densenet121'),
