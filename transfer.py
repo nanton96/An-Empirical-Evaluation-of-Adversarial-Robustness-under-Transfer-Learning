152412e60b64824095913193c1752d77eeae5aef
Korres-Timos <timoskorres@hotmail.com>
2019-04-20 03:13:27 +0100

    Fixed label leaking

--- transfer.py
+++ transfer.py
@@ -32,9 +32,8 @@ experiments =  {
         'transfer_all_layers_densenet121_nat_pgd':   ('densenet121_cifar100',     'densenet121', 'pgd', True, -1),
         'transfer_all_layers_densenet121_pgd_nat':   ('densenet121_cifar100_pgd', 'densenet121', 'nat', False, -1),
         'transfer_all_layers_densenet121_pgd_pgd':   ('densenet121_cifar100_pgd', 'densenet121', 'pgd', True, -1), 
-        'stupid_resnet56_nat_fgsm':       ('resnet56_cifar100',      'resnet56', 'fgsm', True, -1),
-        'stupid_resnet56_nat_pgd':       ('resnet56_cifar100',      'resnet56', 'pgd', True, -1),
-    
+
+        
 
         'transfer_all_layers_resnet56_nat_nat':        ('resnet56_cifar100',      'resnet56', 'nat', False, -1),
         'transfer_all_layers_resnet56_fgsm_fgsm':      ('resnet56_cifar100_fgsm', 'resnet56', 'fgsm', True, -1), 
--- utils/experiment_builder.py
+++ utils/experiment_builder.py
@@ -219,10 +219,8 @@ class ExperimentBuilder(nn.Module):
         e = 0.0625
         advesary =  self.attacker(epsilon = e)
         if advesary.name == 'pgd':
-            print("Indeed pgd")
             y_pred = y.cpu()    
         else:
-            print("Indeed fgsm")
             y_pred  = pred_batch(x,self.model)
 
 
@@ -284,8 +282,9 @@ class ExperimentBuilder(nn.Module):
         validaton_stat['clean_loss'] = np.asscalar(loss.data.detach().cpu().numpy())
         
         # Prevent label leaking, by using most probable state
-
-        if self.attacker.name == 'pgd':
+        e = 0.0625
+        advesary =  self.attacker(epsilon = e)
+        if advesary.name == 'pgd':
             y_pred =  y = y.cpu()    
         else:
             y_pred  = pred_batch(x,self.model)
@@ -300,8 +299,7 @@ class ExperimentBuilder(nn.Module):
         # Create corresponding adversarial examples for training 
 
         # e = self.distribution.rvs(1)[0]
-        e = 0.0625
-        adversary = self.attacker(epsilon = e)
+       
         x_adv = adv_train(x,y_pred, self.model,F.cross_entropy,adversary) 
         x_adv_var = to_var(x_adv)
         out = self.model(x_adv_var)
