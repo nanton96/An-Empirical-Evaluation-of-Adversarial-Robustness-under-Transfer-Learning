9326914e3fee579ca48019126af0bacdc231d6bb
Stathis Fotiadis <s1680171@ed.ac.uk>
2019-04-20 04:19:15 +0100

    Update

--- evaluate_attacks.py
+++ evaluate_attacks.py
@@ -121,7 +121,7 @@ for trained_network, (dataset_name, model) in trained_networks.items():
             acc = attack_over_test_data(model=net,device=device ,adversary=adversary, param=None, loader=test_data, oracle=None)
             results[trained_network+"_attacked_by_"+adversary.name+'_e_%.5f'%e] = acc
 
-    path = './attack_results/white_box/white_box_attacks_pgd_7_original_%s.json' % trained_network
+    path = './attack_results/white_box/white_box_attacks_pgd_7_TEST_CLIP_%s.json' % trained_network
     with open(path, 'w') as outfile:
         json.dump(results, outfile)        
 
