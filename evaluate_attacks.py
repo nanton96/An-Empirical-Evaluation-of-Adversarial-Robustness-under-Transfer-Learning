ed5e7ca8d1ac7846c28e30f2ffb1eab580d0743d
Korres-Timos <timoskorres@hotmail.com>
2019-04-20 01:14:54 +0100

    Testing LL effect

--- evaluate_attacks.py
+++ evaluate_attacks.py
@@ -36,37 +36,43 @@ else:
     device = torch.device('cpu')  # sets the device to be CPU
 
 trained_networks =  {
+
                     # 'resnet56_cifar10': 'cifar10',
                     # 'resnet56_cifar10_fgsm': 'cifar10',
-                    "transfer_densenet121_fgsm_fgsm": ('cifar10', 'densenet121'),
-                    "transfer_densenet121_fgsm_nat": ('cifar10', 'densenet121'),
-                    "transfer_densenet121_nat_fgsm": ('cifar10', 'densenet121'),
-                    "transfer_densenet121_nat_nat": ('cifar10', 'densenet121'),
-                    "transfer_densenet121_nat_pgd": ('cifar10', 'densenet121'),
-                    "transfer_densenet121_pgd_nat": ('cifar10', 'densenet121'),
-                    "transfer_densenet121_pgd_pgd": ('cifar10', 'densenet121'),
-                    "transfer_resnet56_fgsm_fgsm": ('cifar10', 'resnet56'),
-                    "transfer_resnet56_fgsm_nat": ('cifar10', 'resnet56'),
-                    "transfer_resnet56_nat_fgsm": ('cifar10', 'resnet56'),
-                    "transfer_resnet56_nat_nat": ('cifar10', 'resnet56'),
-                    "transfer_resnet56_nat_pgd": ('cifar10', 'resnet56'),
-                    "transfer_resnet56_pgd_nat": ('cifar10', 'resnet56'),
-                    "transfer_resnet56_pgd_pgd": ('cifar10', 'resnet56'),
-                    # BASELINES
+                    # "transfer_densenet121_fgsm_fgsm": ('cifar10', 'densenet121'),
+                    # "transfer_densenet121_fgsm_nat": ('cifar10', 'densenet121'),
+                    # "transfer_densenet121_nat_fgsm": ('cifar10', 'densenet121'),
+                    # "transfer_densenet121_nat_nat": ('cifar10', 'densenet121'),
+                    # "transfer_densenet121_nat_pgd": ('cifar10', 'densenet121'),
+                    # "transfer_densenet121_pgd_nat": ('cifar10', 'densenet121'),
+                    # "transfer_densenet121_pgd_pgd": ('cifar10', 'densenet121'),
+                    # "transfer_resnet56_fgsm_fgsm": ('cifar10', 'resnet56'),
+                    # "transfer_resnet56_fgsm_nat": ('cifar10', 'resnet56'),
+                    # "transfer_resnet56_nat_fgsm": ('cifar10', 'resnet56'),
+                    # "transfer_resnet56_nat_nat": ('cifar10', 'resnet56'),
+                    # "transfer_resnet56_nat_pgd": ('cifar10', 'resnet56'),
+                    # "transfer_resnet56_pgd_nat": ('cifar10', 'resnet56'),
+                    # "transfer_resnet56_pgd_pgd": ('cifar10', 'resnet56'),
 
-                    "densenet121_cifar10": ('cifar10', 'densenet121'),
-                    "densenet121_cifar10_pgd": ('cifar10', 'densenet121'),
-                    "densenet121_cifaf10_fgsm": ('cifar10', 'densenet121'),
-                    "resnet56_cifar10": ('cifar10', 'resnet56'),
-                    "resnet56_cifar10_pgd": ('cifar10', 'resnet56'),
-                    "resnet56_cifaf10_fgsm": ('cifar10', 'resnet56'),
+                    # BASELINES
+                    # "densenet121_cifar10": ('cifar10', 'densenet121'),
+                    # "densenet121_cifar10_pgd": ('cifar10', 'densenet121'),
+                    # "densenet121_cifaf10_fgsm": ('cifar10', 'densenet121'),
+                    # "resnet56_cifar10": ('cifar10', 'resnet56'),
+                    # "resnet56_cifar10_pgd": ('cifar10', 'resnet56'),
+                    # "resnet56_cifaf10_fgsm": ('cifar10', 'resnet56'),
+
+                    "resnet56_cifar10_pgd_tsipras": ('cifar10', 'resnet56'),
+                    "resnet56_cifar10_fgsm_tsipras": ('cifar10', 'resnet56'),
+                    "densenet121_cifar10_fgsm_tsipras": ('cifar10', 'densenet121'),
+                    
                     # CIFAR100
-                    "resnet56_cifar100": ('cifar100', 'resnet56'),
-                    "resnet56_cifar100_pgd": ('cifar100', 'resnet56'),
-                    "resnet56_cifaf100_fgsm": ('cifar100', 'resnet56'),
-                    "densenet121_cifar100": ('cifar100', 'densenet121'),
-                    "densenet121_cifar100_pgd": ('cifar100', 'densenet121'),
-                    "densenet121_cifaf100_fgsm": ('cifar100', 'densenet121'),
+                    # "resnet56_cifar100": ('cifar100', 'resnet56'),
+                    # "resnet56_cifar100_pgd": ('cifar100', 'resnet56'),
+                    # "resnet56_cifaf100_fgsm": ('cifar100', 'resnet56'),
+                    # "densenet121_cifar100": ('cifar100', 'densenet121'),
+                    # "densenet121_cifar100_pgd": ('cifar100', 'densenet121'),
+                    # "densenet121_cifaf100_fgsm": ('cifar100', 'densenet121'),
 
                     # "transfer_all_layers_densenet121_fgsm_fgsm_step_25_gamma_0.1": ('cifar10', 'densenet121'),
                     # "transfer_all_layers_densenet121_fgsm_nat_step_25_gamma_0.1": ('cifar10', 'densenet121'),
@@ -117,7 +123,7 @@ for trained_network, (dataset_name, model) in trained_networks.items():
             acc = attack_over_test_data(model=net,device=device ,adversary=adversary, param=None, loader=test_data, oracle=None)
             results[trained_network+"_attacked_by_"+adversary.name+'_e_%.5f'%e] = acc
 
-    path = './attack_results/white_box/white_box_attacks_pgd_7_baselines_%s.json' % trained_network
+    path = './attack_results/white_box/white_box_attacks_pgd_7_constantEpsilon_LL_%s.json' % trained_network
     with open(path, 'w') as outfile:
         json.dump(results, outfile)        
 
