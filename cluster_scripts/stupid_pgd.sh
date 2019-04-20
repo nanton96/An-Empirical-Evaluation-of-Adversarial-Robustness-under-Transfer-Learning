580c1ca8985a7a87391488f8c5894298ceac5e60
Korres-Timos <timoskorres@hotmail.com>
2019-04-20 02:50:18 +0100

    Testing method

--- cluster_scripts/stupid_pgd.sh
+++ cluster_scripts/stupid_pgd.sh
@@ -40,7 +40,7 @@ cd ..
 
 python transfer.py --batch_size 100 --continue_from_epoch -1 --seed 0 \
 				 --step_size 25 --gamma 0.1 \
-                 --num_epochs 50 \
+                 --num_epochs 1 \
                  --lr 0.1  \
-                 --experiment_name 'transfer_all_layers_resnet56_nat_pgd' \
+                 --experiment_name 'stupid_resnet56_nat_pgd' \
                  --use_gpu True --gpu_id "0" --weight_decay_coefficient 0.00005 
